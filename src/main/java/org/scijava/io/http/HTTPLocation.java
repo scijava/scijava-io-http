3569b55d1179d561866c99227540e50e12143b64
Curtis Rueden <ctrueden@wisc.edu>
2023-08-06 15:52:16 -0500

    Happy New Year 2023

--- src/main/java/org/scijava/io/http/HTTPLocation.java
+++ src/main/java/org/scijava/io/http/HTTPLocation.java
@@ -2,7 +2,7 @@
  * #%L
  * SciJava I/O support for HTTP/HTTPS.
  * %%
- * Copyright (C) 2017 - 2020 SciJava developers.
+ * Copyright (C) 2017 - 2023 SciJava developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
